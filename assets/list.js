1839545bf8bce05a9e00c4bc0be2d14bccfe2d8a
devmentor.pl <55095257+devmentor-pl@users.noreply.github.com>
2022-08-23 12:24:39 +0200

    init

---
+++ assets/list.js
@@ -0,0 +1,50 @@
+const list = [
+    {
+        id: 1,
+        parentId: null,
+        text: 'Woj. kujawsko-pomorskie',
+        ref: 'kuj-pom',
+        description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus eveniet quasi ipsam voluptate, provident ducimus magnam nihil et placeat sint.',
+    },
+    {
+        id: 2,
+        parentId: null,
+        text: 'Woj. pomorskie',
+        ref: 'pomorskie',
+        description:
+            'Voluptates soluta ullam quia eveniet ab perferendis consequuntur sunt? Ducimus voluptates dolorum error molestiae possimus magni magnam quia voluptatibus, doloremque quos, quas nesciunt laudantium deserunt ab.',
+    },
+    {
+        id: 4,
+        parentId: 1,
+        text: 'Toruń',
+        ref: 'torun',
+        description:
+            'Dolore iure corporis maiores natus, possimus quo dolorum quis neque, veniam inventore nam assumenda animi in expedita modi incidunt ipsum vel quaerat molestias, ipsam eveniet illo qui officiis necessitatibus? Necessitatibus libero harum dolore soluta vitae laborum! Repellendus provident vel quos molestias! Voluptates soluta ullam quia eveniet ab perferendis consequuntur sunt',
+    },
+    {
+        id: 5,
+        parentId: null,
+        text: 'Woj. lubelskie',
+        ref: 'lubelskie',
+    },
+    {
+        id: 6,
+        parentId: 1,
+        text: 'Bydgoszcz',
+        ref: 'bydgoszcz',
+        description:
+            'Labore fuga cumque hic illum ad aliquam architecto ipsam asperiores laudantium, commodi rerum itaque. Dolorem cum magni dolor deserunt quas neque earum corporis soluta deleniti, tempora illo natus sapiente. Temporibus necessitatibus commodi laboriosam repudiandae sint corporis aperiam ipsum? Iusto aperiam, odit quis ducimus fugit, minima, debitis doloribus reprehenderit aliquid minus molestias? Voluptates eos officia veniam temporibus nostrum placeat sequi maxime quam animi quia',
+    },
+    {
+        id: 7,
+        parentId: 2,
+        text: 'Gdańsk',
+        ref: 'gdansk',
+        description:
+            'Ducimus voluptates dolorum error molestiae possimus magni magnam quia voluptatibus, doloremque quos, quas nesciunt laudantium deserunt ab! Vel at rerum provident perferendis consequatur doloremque, optio laborum explicabo tempora animi delectus id libero iste voluptates deleniti, alias cum placeat natus unde eligendi excepturi enim nesciunt facilis ea? Voluptatibus quisquam asperiores incidunt pariatur, quia omnis. Deleniti perspiciatis architecto laborum ullam optio odio adipisci',
+    },
+];
+
+export default list
